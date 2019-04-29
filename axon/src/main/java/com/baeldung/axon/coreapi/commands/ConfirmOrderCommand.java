8e86efdac9f464e9ca7732b7bab401a87ee91c4c
Hongyan Li <hongyan99@yahoo.com>
2019-04-28 21:03:17 -0700

    updated from the source and added lombok support to axon

--- axon/src/main/java/com/baeldung/axon/coreapi/commands/ConfirmOrderCommand.java
+++ axon/src/main/java/com/baeldung/axon/coreapi/commands/ConfirmOrderCommand.java
@@ -2,42 +2,11 @@ package com.baeldung.axon.coreapi.commands;
 
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
-import java.util.Objects;
+import lombok.Value;
 
+@Value
 public class ConfirmOrderCommand {
 
     @TargetAggregateIdentifier
-    private final String orderId;
-
-    public ConfirmOrderCommand(String orderId) {
-        this.orderId = orderId;
-    }
-
-    public String getOrderId() {
-        return orderId;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(orderId);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        final ConfirmOrderCommand other = (ConfirmOrderCommand) obj;
-        return Objects.equals(this.orderId, other.orderId);
-    }
-
-    @Override
-    public String toString() {
-        return "ConfirmOrderCommand{" +
-                "orderId='" + orderId + '\'' +
-                '}';
-    }
+    private String orderId;
 }
